0626113925cc7a350f42cb0bdac3bc4b01c41872
Pratik Dhamecha <dhamechapratik5@gmail.com>
2024-11-09 23:56:25 +0530

    Signup & login basic ui done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 // pages/login.js
-
+import { useRouter } from "next/router";
 export default function Login() {
     return (
       <div className="bg-gray-100 flex items-center justify-center min-h-screen">
